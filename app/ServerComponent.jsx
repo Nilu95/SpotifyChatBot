c7b949d56c27eb6c0c55b082fcece3737caf63c9
DylanMill <W0468451@campus.nscc.ca>
2024-03-01 19:26:38 -0400

    Added in Client ID and Client Secret

--- app/ServerComponent.jsx
+++ app/ServerComponent.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ServerComponent = () => {
-  return <h1>Hello world</h1>;
+   // CLIENT CREDENTIALS
+   const client_id = "9224009bb2c14b40886767141ffec1ad";
+   const client_secret = "74d6862034ff450fa04e945de09a1adc";
+
+   return <h1>Hello world</h1>;
 };
 
 export default ServerComponent;
