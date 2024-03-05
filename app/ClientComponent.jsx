91ac97ade8a29870790151a4f2ac541212dcc614
DylanMill <W0468451@campus.nscc.ca>
2024-03-05 15:30:03 -0400

    Getting a token

--- app/ClientComponent.jsx
+++ app/ClientComponent.jsx
@@ -1,22 +1,30 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useTransition, useState } from "react";
 import fetchToken from "./actions/fetchToken";
 
 const ClientComponent = () => {
+   const [isPending, startTransition] = useTransition();
+   const [token, setToken] = useState("null");
+
+   function onClick() {
+      startTransition(() => {
+         fetchToken();
+         setToken(fetchToken);
+      });
+   }
+
    return (
       <>
          <button
-            onClick={fetchToken}
+            type="button"
+            onClick={() => {
+               onClick();
+            }}
             className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
          >
             Request Auth
          </button>
-         <button
-            onClick={skipSong(token)}
-            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-         >
-            Request Auth
-         </button>
+         <h1>Token: {token}</h1>
       </>
    );
 };
--- app/actions/fetchToken.js
+++ app/actions/fetchToken.js
@@ -1,24 +1,27 @@
 "use server";
 
-const fetchToken = async () => {
-   // try {
-   //    const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
-   //       method: "POST",
-   //       headers: {
-   //          "Content-Type": "application/x-www-form-urlencoded",
-   //          Authorization: "Basic " + btoa(client_id + ":" + client_secret),
-   //       },
-   //       body: "grant_type=client_credentials",
-   //    });
+// CLIENT CREDENTIALS
+const client_id = "9224009bb2c14b40886767141ffec1ad";
+const client_secret = "74d6862034ff450fa04e945de09a1adc";
 
-   //    const token = await tokenResponse.json();
-   //    console.log(token);
-   //    return token;
-   // } catch (error) {
-   //    console.error("Error fetching token:", error);
-   //    return null;
-   // }
-   alert("HELLO");
+const fetchToken = async () => {
+   try {
+      const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: "Basic " + btoa(client_id + ":" + client_secret),
+         },
+         body: "grant_type=client_credentials",
+      });
+
+      const token = await tokenResponse.json();
+      console.log(token);
+      return token.access_token;
+   } catch (error) {
+      console.error("Error fetching token:", error);
+      return null;
+   }
 };
 
 export default fetchToken;
