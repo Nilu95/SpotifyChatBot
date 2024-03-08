c4d9570b6e69daf31ff56c246b41280fe3400ee4
DylanMill <W0468451@campus.nscc.ca>
2024-03-08 14:55:46 -0400

    Commit

--- app/ClientComponent.jsx
+++ app/ClientComponent.jsx
@@ -6,27 +6,21 @@ import requestAuth from "./actions/requestAuth";
 import fetchSongData from "./actions/fetchSongData";
 
 const ClientComponent = () => {
-  const [isPending, startTransition] = useTransition();
-  const [token, setToken] = useState("null");
-  const [code, setCode] = useState("code");
+   const [isPending, startTransition] = useTransition();
+   const [token, setToken] = useState("null");
+   const [code, setCode] = useState("code");
 
-  useEffect(() => {
-    // Function to extract code from URL and call fetchToken
-    const getCodeFromURL = () => {
-      const urlParams = new URLSearchParams(window.location.search);
-      const code = urlParams.get("code");
-      if (code) {
-        setCode(code);
-      }
-    };
-    getCodeFromURL();
-  }, [code]); // Empty dependency array to run only once when component mounts
-
-<<<<<<< HEAD
-  useEffect(() => {
-    setToken(fetchToken(code));
-  }, [code]);
-=======
+   useEffect(() => {
+      // Function to extract code from URL and call fetchToken
+      const getCodeFromURL = () => {
+         const urlParams = new URLSearchParams(window.location.search);
+         const code = urlParams.get("code");
+         if (code) {
+            setCode(code);
+         }
+      };
+      getCodeFromURL();
+   }, [code]); // Empty dependency array to run only once when component mounts
    useEffect(() => {
       const fetchTokenAndUpdate = async () => {
          if (code) {
@@ -37,42 +31,12 @@ const ClientComponent = () => {
 
       fetchTokenAndUpdate();
    }, [code]);
->>>>>>> 5d7214b7a13d255987bc7fbca13dfbd69175dbb8
 
-  function onAuthClick() {
-    startTransition(() => {
-      requestAuth();
-    });
-  }
-
-<<<<<<< HEAD
-    function handleSkipSong() {
-      console.log("Hello Token:", token);
-      skipSong("Bearer " + token);
-    }
-
-
-
-  return (
-    <>
-      <button
-        type="button"
-        onClick={() => {
-          onAuthClick();
-        }}
-        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-      >
-        Request Auth
-      </button>
-
-      <button type="button" onClick={handleSkipSong}>
-        SKIP SONG
-      </button>
-      <h1>Token: {token}</h1>
-      <h1>Code: {code}</h1>
-    </>
-  );
-=======
+   function onAuthClick() {
+      startTransition(() => {
+         requestAuth();
+      });
+   }
    return (
       <>
          <button
@@ -106,7 +70,6 @@ const ClientComponent = () => {
          <h1>Code: {code}</h1>
       </>
    );
->>>>>>> 5d7214b7a13d255987bc7fbca13dfbd69175dbb8
 };
 
 export default ClientComponent;
--- app/actions/skipSong.js
+++ app/actions/skipSong.js
@@ -1,18 +1,6 @@
 "use server";
 
 const skipSong = async (access_token) => {
-
-  const response = await fetch("https://api.spotify.com/v1/me/player/next", {
-    method: "POST",
-    headers: {
-      Authorization: "Bearer " + access_token.value,
-    },
-  });
-
-  const data = await response.json();
-  console.log(data);
-  console.log(access_token);
-
    const fetchConfig = {
       method: "POST",
       headers: {
@@ -27,7 +15,6 @@ const skipSong = async (access_token) => {
    console.log(access_token);
    console.log(fetchConfig);
    //console.log(data, "\n");
-
 };
 
 export default skipSong;
