eeea61bddf493fcd9c8160304dfeb0d32a6df03b
DylanMill <W0468451@campus.nscc.ca>
2024-03-08 14:47:33 -0400

    Commit

--- app/actions/skipSong.js
+++ app/actions/skipSong.js
@@ -23,7 +23,7 @@ const skipSong = async (access_token) => {
    const response = await fetch("https://api.spotify.com/v1/me/player/next", fetchConfig);
 
    // const data = await response.json();
-   // console.log(response);
+   console.log(response);
    console.log(access_token);
    console.log(fetchConfig);
    //console.log(data, "\n");
