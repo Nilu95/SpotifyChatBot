6fc52355b690f92609cc919fe61c78159c97c173
DylanMill <W0468451@campus.nscc.ca>
2024-03-08 22:30:35 -0400

    Removed the need to manually request the song data

--- app/ClientComponent.jsx
+++ app/ClientComponent.jsx
@@ -97,15 +97,6 @@ const ClientComponent = () => {
          >
             SKIP SONG
          </button>
-         <button
-            type="button"
-            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-            onClick={() => {
-               getSongData();
-            }}
-         >
-            FETCH SONG DATA
-         </button>
       </>
    );
 };
